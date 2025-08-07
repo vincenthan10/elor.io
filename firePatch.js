62f609ca3be80ca15d5efcdc2535dc1faedd2821
vincenthan10 <113260293+vincenthan10@users.noreply.github.com>
2025-08-07 10:42:58 -0400

    HP update

--- firePatch.js
+++ firePatch.js
@@ -6,6 +6,10 @@ class FirePatch {
         this.isAlive = true;
         this.isFading = false;
         this.fadeAlpha = 1; // for fade-out animation
+        this.maxHp = 7;
+        this.hp = this.maxHp;
+
+        this.hitCooldown = 0;
     }
 
     update(deltaTime) {
@@ -14,6 +18,10 @@ class FirePatch {
             if (this.fadeAlpha <= 0) {
                 this.isAlive = false;
             }
+        }
+
+        if (this.hitCooldown > 0) {
+            this.hitCooldown -= deltaTime;
         }
     }
 
@@ -29,12 +37,25 @@ class FirePatch {
         ctx.arc(this.x - camera.x, this.y - camera.y, flickerSize, 0, Math.PI * 2);
         ctx.fillStyle = `hsl(${Math.random() * 30}, 100%, 50%)`; //yellow/orange
         ctx.fill();
-
         ctx.restore();
+
+
+        // HP bar
+        ctx.fillStyle = "red";
+        ctx.fillRect(this.x - camera.x - this.radius, this.y - camera.y + this.radius * 1.25, this.radius * 2, this.radius / 5);
+
+        ctx.fillStyle = "limegreen";
+        ctx.fillRect(this.x - camera.x - this.radius, this.y - camera.y + this.radius * 1.25, (this.hp / this.maxHp) * (this.radius * 2), this.radius / 5);
+
     }
 
-    hit() {
-        if (this.isFading) return;
-        this.isFading = true;
+    hit(damage) {
+        if (this.isFading || this.hitCooldown > 0) return;
+        this.hp -= damage;
+        this.hitCooldown = 250;
+        if (this.hp <= 0) {
+            this.hp = 0;
+            this.isFading = true;
+        }
     }
 }
