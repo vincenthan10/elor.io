1124de75bb1f3ed3d35d39e0799fda728a4f7383
vincenthan10 <113260293+vincenthan10@users.noreply.github.com>
2025-09-06 14:30:55 -0400

    Started reorganizing

--- player.js
+++ player.js
@@ -1,14 +1,9 @@
-class Player {
+class Player extends Entity {
     constructor(x, y) {
-        this.x = x;
-        this.y = y;
-        this.radius = 20;
+        super(x, y, 20);
+        this.damageable = new Damageable(10);
         this.speed = 2.5;
         this.aimAngle = 0;
-        this.fireShards = 0;
-        this.maxHp = 10;
-        this.hp = this.maxHp;
-        this.hpPerc = this.hp / this.maxHp;
         this.level = 1;
         this.xp = 0;
         this.xpNeeded = 5;
@@ -22,23 +17,11 @@ class Player {
         this.STRENGTH_GROWTH_RATE = 1.025;
         this.skillPoints = 0;
 
-        // Sword animation and stats
-        this.isSwinging = false;
-        this.swingTimer = 0;
-        this.swingDuration = 250;
-        this.swingAngleOffset = 0;
-        this.swingCooldown = 500;
-        this.lastSwingTime = 0;
-        this.swordDamage = 3;
+        // Sword 
+        this.sword = new Sword();
+        this.shield = new Shield();
 
         this.damage = this.strength + this.swordDamage;
-
-        // Shield animation
-        this.isBlocking = false;
-        this.blockTimer = 0;
-        this.blockDuration = 750;
-        this.blockCooldown = 1500;
-        this.lastBlockTime = 0;
 
         // Mouse movement
         this.dx = 0;
@@ -88,6 +71,24 @@ class Player {
         }
     }
 
+    attack() {
+        const now = performance.now();
+        if (!this.sword.isSwinging && now - this.sword.lastSwingTime >= this.sword.timeSinceLastSwing) {
+            this.sword.isSwinging = true;
+            this.sword.swingTimer = 0;
+            this.sword.timeSinceLastSwing = now;
+        }
+    }
+
+    defend() {
+        const now = performance.now();
+        if (!this.shield.isBlocking && now - this.shield.lastBlockTime >= this.shield.blockCooldown) {
+            this.shield.isBlocking = true;
+            this.shield.blockTimer = 0;
+            this.shield.lastBlockTime = now;
+        }
+    }
+
     update(deltaTime, keysPressed, camera, mapWidth, mapHeight, isCollidingWithWall) {
         // Movement and boundaries
         //keyboard movement
@@ -134,8 +135,6 @@ class Player {
             if (!isCollidingWithWall(this.x, newY)) this.y = newY;
         }
 
-
-
         camera.x = this.x - canvas.width / 2;
         camera.y = this.y - canvas.height / 2;
 
@@ -143,34 +142,12 @@ class Player {
         camera.x = Math.max(0, Math.min(camera.x, mapWidth - canvas.width));
         camera.y = Math.max(0, Math.min(camera.y, mapHeight - canvas.height));
 
+        this.sword.swing(deltaTime, this);
+        this.shield.block(deltaTime);
         this.damage = this.strength + this.swordDamage;
-
-        // sword animation
-        if (this.isSwinging) {
-            this.swingTimer += deltaTime;
-            const progress = this.swingTimer / this.swingDuration;
-
-            // animate an arc from -45 degrees to +45 degrees relative to player aim
-            const maxOffset = Math.PI / 4;
-            this.swingAngleOffset = -maxOffset + (progress * 2 * maxOffset);
-
-            if (progress >= 1) {
-                this.isSwinging = false;
-                this.swingAngleOffset = 0;
-            }
-        }
-
-        // shield animation
-        if (this.isBlocking) {
-            this.blockTimer += deltaTime;
-            if (this.blockTimer >= this.blockDuration) {
-                this.isBlocking = false;
-            }
-        }
 
         this.checkLevelUp();
         this.passiveRegen(deltaTime);
-        this.hpPerc = this.hp / this.maxHp;
 
     }
 
@@ -182,94 +159,6 @@ class Player {
         ctx.fillStyle = "green";
         ctx.fill();
         ctx.closePath();
-
-        // SWORD
-        const swordLength = 30;
-        const swordWidth = 12;
-        const angle = this.aimAngle;
-        const swordAngle = this.aimAngle + this.swingAngleOffset;
-
-        // offset base of sowrd to the right of the player's body
-        const handOffsetAngle = swordAngle + Math.PI / 2.1;
-        const handOffsetDistance = this.radius * 1.1;
-
-        // tip of sword
-        const tipX = this.x + Math.cos(swordAngle) * (this.radius + swordLength);
-        const tipY = this.y + Math.sin(swordAngle) * (this.radius + swordLength);
-
-        // base of the sword
-        const baseX = this.x + Math.cos(handOffsetAngle) * handOffsetDistance;
-        const baseY = this.y + Math.sin(handOffsetAngle) * handOffsetDistance;
-
-        // perpendicular offsets for triangle width
-        const offsetX = Math.cos(swordAngle + Math.PI / 2) * (swordWidth / 2);
-        const offsetY = Math.sin(swordAngle + Math.PI / 2) * (swordWidth / 2);
-
-        // base corners
-        const base1X = baseX + offsetX;
-        const base1Y = baseY + offsetY;
-
-        const base2X = baseX - offsetX;
-        const base2Y = baseY - offsetY;
-
-        // draw triangle
-        ctx.fillStyle = "silver";
-        ctx.beginPath();
-        ctx.moveTo(tipX - camera.x, tipY - camera.y);
-        ctx.lineTo(base1X - camera.x, base1Y - camera.y);
-        ctx.lineTo(base2X - camera.x, base2Y - camera.y);
-        ctx.closePath();
-        ctx.fill();
-
-
-        // SHIELD (custom pentagon: triangle on top of square)
-        const shieldWidth = 22;
-        const shieldHeight = 25;
-        const shieldTipHeight = 10;
-        const halfW = shieldWidth / 2;
-
-        const shieldLocalPoints = [
-            { x: shieldHeight + shieldTipHeight, y: 0 },   // tip (right side)
-            { x: shieldHeight, y: -halfW },                // top right
-            { x: 0, y: -halfW },                            // top left
-            { x: 0, y: halfW },                             // bottom left
-            { x: shieldHeight, y: halfW }                  // bottom right
-        ];
-
-        const isBlocking = this.isBlocking;
-        // if isBlocking, calculate the progress, otherwise, progress is 0
-        const blockProgress = isBlocking ? Math.min(this.blockTimer / this.blockDuration, 1) : 0;
-        const blockScale = 1 + 0.3 * Math.sin(blockProgress * Math.PI); // scale to grow and shrink when blocking
-
-        const offsetAngle = angle - Math.PI / 2.1;
-        const offsetDistance = this.radius * 1.1;
-
-        const shieldCenterX = this.x + Math.cos(offsetAngle) * offsetDistance;
-        const shieldCenterY = this.y + Math.sin(offsetAngle) * offsetDistance;
-
-        const cos = Math.cos(angle);
-        const sin = Math.sin(angle);
-
-        ctx.fillStyle = isBlocking ? "#88ffff" : "#88aaff";
-        ctx.save();
-        ctx.translate(shieldCenterX - camera.x, shieldCenterY - camera.y);
-        ctx.rotate(this.aimAngle);
-        ctx.scale(blockScale, blockScale); //scale relative to center
-        ctx.beginPath();
-
-        for (let i = 0; i < shieldLocalPoints.length; i++) {
-            const local = shieldLocalPoints[i];
-
-
-            if (i === 0) {
-                ctx.moveTo(local.x, local.y);
-            } else {
-                ctx.lineTo(local.x, local.y);
-            }
-        }
-        ctx.closePath();
-        ctx.fill();
-        ctx.restore();
 
         // HP bar
         ctx.fillStyle = "red";
