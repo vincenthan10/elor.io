25aa7f7288210b934f9396596b3ea788cd03150c
vincenthan10 <113260293+vincenthan10@users.noreply.github.com>
2025-09-11 10:53:00 -0400

    Update inventory.js

--- inventory.js
+++ inventory.js
@@ -31,8 +31,23 @@ class Inventory extends Menu {
         ctx.font = "18px Arial";
         ctx.fillText("Inventory", invX + 10, invY + 25);
 
-        ctx.font = "14px Arial";
+        let offsetY = 50;
+        for (const key in this.player.inventory) {
+            const amount = this.player.inventory[key].amount;
 
+            // Try to get class from registry
+            const ItemClass = ItemRegistry[key];
+            if (ItemClass && typeof ItemClass.prototype.drawIcon === "function") {
+                const iconX = invX + 10;
+                const iconY = invY + offsetY - 10;
+                ItemClass.drawIcon(ctx, iconX, iconY);
+            }
+        }
+        ctx.fillStyle = "white";
+        ctx.font = "14px Arial";
+        ctx.fillText(`${key}: ${amount}`, invX, invY + offsetY);
+
+        offsetY += 30;
 
     }
 }
