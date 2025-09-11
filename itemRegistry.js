271a844155c849d668f76db058b89cbdbf99ed1d
vincenthan10 <113260293+vincenthan10@users.noreply.github.com>
2025-09-11 10:53:59 -0400

    Facilitates drawing icons in Inventory

---
+++ itemRegistry.js
@@ -0,0 +1,9 @@
+import FireShard from "./fireShard.js";
+import Sword from "./sword.js";
+import Shield from "./shield.js";
+
+export const ItemRegistry = {
+    "Fire Shard": FireShard,
+    "Basic Sword": Sword,
+    "Basic Shield": Shield
+}
