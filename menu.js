ee76c2bcee3c65591209b7d3c6bbb9736a67cc7e
vincenthan10 <113260293+vincenthan10@users.noreply.github.com>
2025-09-13 10:42:13 -0400

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,15 +1,10 @@
 class Menu {
     constructor(options = []) {
-        this.options = options;
         this.selectedIndex = 0;
         this.isOpen = false;
     }
     open() { this.isOpen = true; }
     close() { this.isOpen = false; }
-
-    select() {
-        if (this.isOpen) this.options[this.selectedIndex].action();
-    }
 
     draw(ctx) {
     }
