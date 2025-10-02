d2d6c08381cfe1662bdf223acc27b7e9b849d429
vincenthan10 <113260293+vincenthan10@users.noreply.github.com>
2025-10-02 14:23:17 -0400

    Update itemRegistry.js

--- itemRegistry.js
+++ itemRegistry.js
@@ -3,7 +3,19 @@ import Sword from "./sword.js";
 import Shield from "./shield.js";
 
 export const ItemRegistry = {
-    "Fire Shard": FireShard,
-    "Basic Sword": Sword,
-    "Basic Shield": Shield
+    fireShard: {
+        class: FireShard,
+        name: "Fire Shard",
+        drawIcon: FireShard.drawIcon
+    },
+    basicSword: {
+        class: Sword,
+        name: "Basic Sword",
+        drawIcon: Sword.drawIcon
+    },
+    basicShield: {
+        class: Shield,
+        name: "Basic Shield",
+        drawIcon: Shield.drawIcon
+    }
 }
