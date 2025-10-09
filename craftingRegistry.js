a533a2e6af97fbd646ff60de578e5bdeb55a68cf
vincenthan10 <113260293+vincenthan10@users.noreply.github.com>
2025-10-09 15:03:06 -0400

    New: Red Shield (1.5x Fire Sword cost)

--- craftingRegistry.js
+++ craftingRegistry.js
@@ -3,5 +3,10 @@ export const CraftingRegistry = {
         name: "Fire Sword",
         requires: { fireShard: 2 },
         produces: { fireSword: 1 }
+    },
+    redShield: {
+        name: "Red Shield",
+        requires: { fireShard: 3 },
+        produces: { redShield: 1 }
     }
 };
